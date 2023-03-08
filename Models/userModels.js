fdce3e82421f291d1046347ec492b0c19bb91de9
GaelTesson <gael@tesson.me>
2023-03-08 14:38:23 +0100

    try address

--- Models/userModels.js
+++ Models/userModels.js
@@ -21,24 +21,22 @@ const userSchema =new mongoose.Schema({
         trim: true,
         lowercase: true
     },
+        street: {
+            type: String,
+            required: true,
+            trim: true
+        },
 
-    // address: {
-    //     street: {
-    //         type: String,
-    //         required: true,
-    //         trim: true
-    //     },
-
-    //     city: {
-    //         type: String,
-    //         required: true,
-    //         trim: true
-    //     },
-    //     zipcode: {
-    //         type: Number,
-    //         required: true,
-    //         trim: true
-    // }},
+        city: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        zipcode: {
+            type: Number,
+            required: true,
+            trim: true
+    },
 
     birthdate: {
         type: String,
